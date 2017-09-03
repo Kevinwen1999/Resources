dfe0802ed4cd2f03659ff3d41539dc13defe2b78
Kevin Wen <cybergodzilla1999@gmail.com>
2017-09-03 15:39:50 -0400

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -20,3 +20,42 @@ void add_word(string &s, int v) {
     }
     val[u] = v;
 }
+// Left Child Right Siblings Version
+int sz = 0, root;
+struct TrieNode {
+    char chr;
+    int lchild; // pointing to first child
+    int rchild; // pointing to adjacent siblings
+};
+TrieNode trie[MAXNODE];
+int NewNode(char c) {
+    trie[sz].lchild = trie[sz].rchild = -1;
+    trie[sz++].chr = c;
+    return sz - 1;
+}
+void init_trie() {
+    sz = 0;
+    root = NewNode(0);
+}
+void TrieInsert(char *s) {
+    int slen = strlen(s);
+    int cur = trie[root].lchild; // current node
+    int par = root; // parent of cur
+    bool same = true;
+    for (int i = 0; i <= slen; i++) {
+        if (cur == -1) { // current node for s[i] DNE
+            trie[par].lchild = cur = NewNode(s[i]);
+            // modify value associated with cur here
+        } else {
+            // modify value associated with cur here
+            while (trie[cur].rchild != -1 && trie[cur].chr != s[i]) // keep going right to siblings
+                cur = trie[cur].rchild;
+            if (trie[cur].chr != s[i]) { // if corresponding node is not found
+                trie[cur].rchild = NewNode(s[i]);
+                cur = trie[cur].rchild;
+            }
+        }
+        par = cur;
+        cur = trie[par].lchild; // keep going down
+    }
+}
