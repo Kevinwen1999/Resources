7c88d033086bb93bd4878b1797b0099f6b9cf65a
Kevin Wen <cybergodzilla1999@gmail.com>
2017-11-11 18:36:56 -0500

    Update sparse-table.cpp

--- sparse-table.cpp
+++ sparse-table.cpp
@@ -32,3 +32,18 @@ public:
 	} 
 
 };
+
+void buildSPT(int n, int A[]) {
+    for (int i = 0; i < n; i++) {
+        SPT[i][0] = i;
+    }
+    for (int j = 1; (1 << j) <= n; j++) {
+        for (int i = 0; i + (1 << j) - 1 < n; i++) {
+            SPT[i][j] = (A[SPT[i][j - 1]] < A[SPT[i + (1 << (j - 1))][j - 1]]) ? SPT[i][j - 1] : SPT[i + (1 << (j - 1))][j - 1];
+        }
+    }
+}
+int querySPT(int i, int j, int A[]) {
+    int k = log((double)(j - i + 1)) / log(2.0);
+    return (A[SPT[i][k]] < A[SPT[j - (1 << k) + 1][k]]) ? SPT[i][k] : SPT[j - (1 << k) + 1][k];
+}
