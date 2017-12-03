35161bab12135216f52ddae676a2e423f8a869b3
Kevin Wen <cybergodzilla1999@gmail.com>
2017-12-03 00:17:58 -0500

    Create centroid.cpp

---
+++ centroid.cpp
@@ -0,0 +1,41 @@
+vector <int> adj[MAXN], cadj[MAXN];
+bool visited[MAXN], marked[MAXN];
+int subsz[MAXN];
+int ntotal;
+void calc_sz(int src, int pa) {
+    ntotal ++;
+    subsz[src] = 1;
+    for (int i : adj[src]) {
+        if (i == pa || marked[i]) continue;
+        calc_sz(i, src);
+        subsz[src] += subsz[i];
+    }
+}
+int findct(int src, int pa) {
+    bool OK = true;
+    int hvc = -1;
+    for (int i : adj[src]) {
+        if (i == pa || marked[i]) continue;
+        if (subsz[i] > ntotal / 2) OK = false;
+        if (hvc == -1 || subsz[hvc] < subsz[i])
+            hvc = i;
+    }
+    if (OK && ntotal - subsz[src] <= ntotal / 2)
+        return src;
+    else
+        return findct(hvc, src);
+}
+int ctdecomp(int src) {
+    ntotal = 0;
+    calc_sz(src, -1);
+    int ct = findct(src, -1);
+    marked[ct] = true;
+    for (int i : adj[ct]) {
+        if (!marked[i]) {
+            int cst = ctdecomp(i);
+            cadj[ct].push_back(cst);
+            cadj[cst].push_back(ct);
+        }
+    }
+    return ct;
+}
